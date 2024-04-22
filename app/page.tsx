f7e2ccef6be0991c9a572821862b591967a57983
Lim <oncwnuNmqDLfBRgceXWI_G4BisFw@git.weixin.qq.com>
2024-04-22 20:06:19 +0800

    feat: style

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ import { useState } from 'react';
 import CodeEditor, { loader } from '@monaco-editor/react';
 import * as monaco from 'monaco-editor';
 loader.config({ monaco });
+import './page.css';
 
-const CODE_EDITOR_HEIGHT = '30vh';
+const CODE_EDITOR_HEIGHT = '50vh';
 const CODE_EDITOR_THEME = 'vs-dark';
 const CODE_EDITOR_OPTIONS = {
   fontSize: 18 
@@ -17,7 +18,7 @@ export default function Home() {
   const renderStr = ` ${html} <style>${css}</style> `;
 
   return (
-    <main className="flex flex-col p-10 h-full bg-slate-800">
+    <main className="container flex flex-col p-10 h-full bg-slate-800">
       <div className="flex gap-4">
         {/* HTML 编辑器 */}
         <div className="flex-1">
@@ -53,8 +54,7 @@ export default function Home() {
       {/* 页面渲染 */}
       <div
         dangerouslySetInnerHTML={{ __html: renderStr }}
-        className="flex-1 p-4 mt-4 text-white"
-        style={{ backgroundColor: "rgb(30, 30, 30)" }}
+        className="content flex-1 p-4 mt-4 text-white bg-gray-300"
       ></div>
     </main>
   );
