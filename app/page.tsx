309b97b3c317335d648d9afdd24f46c5520f20a3
Lim <oncwnuNmqDLfBRgceXWI_G4BisFw@git.weixin.qq.com>
2024-04-22 17:33:18 +0800

    feat: style

--- app/page.tsx
+++ app/page.tsx
@@ -5,40 +5,57 @@ import CodeEditor, { loader } from '@monaco-editor/react';
 import * as monaco from 'monaco-editor';
 loader.config({ monaco });
 
+const CODE_EDITOR_HEIGHT = '30vh';
+const CODE_EDITOR_THEME = 'vs-dark';
+const CODE_EDITOR_OPTIONS = {
+  fontSize: 18 
+};
+
 export default function Home() {
   const [html, setHTML] = useState('');
   const [css, setCSS] = useState('');
   const renderStr = ` ${html} <style>${css}</style> `;
 
   return (
-    <main>
-      <div className="flex">
+    <main className="flex flex-col p-10 h-full bg-slate-800">
+      <div className="flex gap-4">
         {/* HTML 编辑器 */}
-        <div>
+        <div className="flex-1">
+          <span className="text-gray-400">
+            HTML
+          </span>
           <CodeEditor
-            width="50vw"
-            height="50vh"
-            defaultLanguage='html'
-            theme="vs-dark"
             value={html}
-            onChange={(value: string) => setHTML(value || '')}
+            onChange={value => setHTML(value || '')}
+            defaultLanguage='html'
+            height={CODE_EDITOR_HEIGHT}
+            theme={CODE_EDITOR_THEME}
+            options={CODE_EDITOR_OPTIONS}
+            className="text-5xl"
           />
         </div>
 
-        <div>
+        <div className="flex-1">
+          <span className="text-gray-400">
+            CSS 
+          </span>
           {/* CSS 编辑器 */}
           <CodeEditor
-            width="50vw"
-            height="50vh"
-            defaultLanguage="css"
-            theme="vs-dark"
             value={css}
-            onChange={(value: string) => setCSS(value || '')}
+            onChange={value => setCSS(value || '')}
+            height={CODE_EDITOR_HEIGHT}
+            theme={CODE_EDITOR_THEME}
+            defaultLanguage="css"
+            options={CODE_EDITOR_OPTIONS}
           />
         </div>
       </div>
       {/* 页面渲染 */}
-      <div dangerouslySetInnerHTML={{ __html: renderStr }}></div>
+      <div
+        dangerouslySetInnerHTML={{ __html: renderStr }}
+        className="flex-1 p-4 mt-4 text-white"
+        style={{ backgroundColor: "rgb(30, 30, 30)" }}
+      ></div>
     </main>
   );
 }
